a08e6c20955a6651f5dc6648d4ac0a6557693a93
mick-25 <158113781+mick-25@users.noreply.github.com>
2024-03-05 07:55:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -478,7 +478,7 @@ let fetchTorrent = async (query, type = "series") => {
       : type == "movie"
       ? "&Category%5B%5D=2000"
       : ""
-  }&Category%5B%5D=8000&Tracker%5B%5D=yggtorrent&cache=false`;
+  }&Category%5B%5D=8000&Tracker%5B%5D=wolfmax4k&cache=false`;
 
   return await fetch(url, {
     headers: {
